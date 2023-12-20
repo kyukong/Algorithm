724e14f3f616667d1cc5a13f1abb3d435ca2d463
kyukong <92148749+kyukong@users.noreply.github.com>
2023-12-20 11:34:31 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,8 +1,13 @@
-select b.TITLE, b.BOARD_ID, r.REPLY_ID, r.WRITER_ID, r.CONTENTS, 
-    to_char(r.CREATED_DATE, 'YYYY-MM-DD') as CREATED_DATE
-from USED_GOODS_BOARD b
-inner join USED_GOODS_REPLY r
+select
+    b.title title,
+    b.board_id board_id,
+    r.reply_id reply_id,
+    r.writer_id writer_id,
+    r.contents contents,
+    to_char(r.created_date, 'YYYY-MM-DD') created_date
+from used_goods_board b
+join used_goods_reply r
 on b.board_id = r.board_id
-where to_char(b.CREATED_DATE, 'YYYY-MM') = '2022-10'
-order by r.CREATED_DATE, b.TITLE
+where to_char(b.created_date, 'YYYY-MM') = '2022-10'
+order by r.created_date, b.title
 ;
