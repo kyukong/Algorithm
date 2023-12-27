e6b2551826e1c8cd6cba8944d00c753da601391e
kyukong <92148749+kyukong@users.noreply.github.com>
2023-12-27 09:21:47 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,15 @@
+select 
+    '/home/grep/src/'
+        || board_id || '/'
+        || file_id
+        || file_name 
+        || file_ext FILE_PATH
+from used_goods_file
+where board_id = (
+    select board_id
+    from used_goods_board
+    where views = (
+        select max(views)
+        from used_goods_board))
+order by file_id desc
+;
