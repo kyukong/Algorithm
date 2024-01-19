6ad3fd049fad667d8ca4baf9ee5d399cfce63aa7
kyukong <92148749+kyukong@users.noreply.github.com>
2024-01-19 11:12:19 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,11 +1,8 @@
-select hour, count(*) count
-from (
-    select 
-        datetime, 
-        extract(hour from cast(datetime as timestamp)) hour
-    from animal_outs
-    where extract(hour from cast(datetime as timestamp))
-        between 9 and 19)
-group by hour
-order by hour
+select extract(hour from cast(datetime as timestamp)) hour,
+    count(*) count
+from animal_outs
+where extract(hour from cast(datetime as timestamp))
+    between 9 and 19
+group by extract(hour from cast(datetime as timestamp))
+order by extract(hour from cast(datetime as timestamp))
 ;
