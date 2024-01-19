b9f06dc8edab3c6e3418c8ba69d12065850fd22b
kyukong <92148749+kyukong@users.noreply.github.com>
2024-01-19 11:14:02 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,7 +1,4 @@
-select
-    animal_type, 
-    nvl(name, 'No name') name,
-    sex_upon_intake
+select animal_type, nvl(name, 'No name') name, sex_upon_intake
 from animal_ins
 order by animal_id
 ;
