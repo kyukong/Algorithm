c1f96dd10e599c677d3c662928648fd511ca1ecf
kyukong <92148749+kyukong@users.noreply.github.com>
2024-03-05 01:03:31 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/README.md
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 중성화 여부 파악하기 - 59409 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59409?language=oracle) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59409) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 1월 2일 9:31:53
+2024년 03월 05일 01:03:27
 
 ### 문제 설명
 
--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,9 +1,7 @@
-select 
-    animal_id,
-    name,
-    case when sex_upon_intake like '%Neutered%'then 'O'
-         when sex_upon_intake like '%Spayed%' then 'O'
-         else 'X' end "중성화"
-from animal_ins
-order by animal_id
+SELECT ANIMAL_ID, NAME, 
+    CASE WHEN SEX_UPON_INTAKE LIKE '%Neutered%' THEN 'O'
+        WHEN SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+        ELSE 'X' END "중성화"
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
 ;
