29f68d780566322c588c4d94132fdc2ddcd9883b
kyukong <92148749+kyukong@users.noreply.github.com>
2024-03-05 01:33:54 +0900

    [level 2] Title: 타겟 넘버, Time: 0.46 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,27 +1,26 @@
 class Solution {
-
+    
     private int[] numbers;
     private int target;
     private int result = 0;
-
+    
     public int solution(int[] numbers, int target) {
         this.numbers = numbers;
         this.target = target;
-
+    
         dfs(0, 0);
-
+        
         return result;
     }
-
+    
     private void dfs(int now, int i) {
-
         if (i >= numbers.length) {
             if (now == target) {
                 result++;
             }
             return;
         }
-
+        
         dfs(now + numbers[i], i + 1);
         dfs(now - numbers[i], i + 1);
     }
