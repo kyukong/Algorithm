626559267a67ea82a37d2604694fb7ea80041e31
kyukong <92148749+kyukong@users.noreply.github.com>
2024-03-05 11:22:37 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.37 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,57 +2,55 @@ import java.util.*;
 
 class Solution {
     
-    private static int[][] visited;
-    private static int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    private static final int[][] directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}; // x, y
+    
+    private int[][] maps;
+    private int[][] visited;
+    private int width;
+    private int height;
     
     public int solution(int[][] maps) {
+        this.maps = maps;
+        this.width = maps[0].length; // x
+        this.height = maps.length; // y
+        this.visited = new int[height][width];
         
-        int height = maps.length;
-        int width = maps[0].length;
+        bfs();
         
-        visited = new int[height][width];
-        
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {0, 0});
-        visited[0][0] = 1;
-        
-        int nextx, nexty;
-        while (!q.isEmpty()) {
-            
-            int[] point = q.poll();
-            
-            for (int[] direction : directions) {
-                nextx = point[1] + direction[1];
-                nexty = point[0] + direction[0];
-                
-                if (nextx < 0 || nexty < 0 || nextx >= width || nexty >= height) {
-                    continue;
-                }
-                if (maps[nexty][nextx] == 0) {
-                    continue;
-                }
-                if (visited[nexty][nextx] != 0) {
-                    continue;
-                }
-                
-                visited[nexty][nextx] = visited[point[0]][point[1]] + 1;
-                
-                if (nextx == width - 1 && nexty == height - 1) {
-                    break;
-                }
-                q.offer(new int[] {nexty, nextx});
-            }
-        
-            // for (int i = 0; i < maps.length; i++) {
-            //     System.out.println(Arrays.toString(visited[i]));
-            // }
-            // System.out.println("===");
-        }
-        
-        int result = visited[height - 1][width - 1];
-        if (result == 0) {
+        if (visited[height - 1][width - 1] == 0) {
             return -1;
         }
-        return result;
+        return visited[height - 1][width - 1];
+    }
+    
+    private void bfs() {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {0, 0}); // x, y
+        visited[0][0] = 1;
+        
+        while (!q.isEmpty()) {
+            int[] point = q.poll();
+            
+            if (point[0] == height - 1 && point[1] == width - 1) {
+                break;
+            }
+            
+            for (int[] direction : directions) {
+                int nextX = point[0] + direction[0];
+                int nextY = point[1] + direction[1];
+                
+                if (nextX < 0 || nextY < 0 || nextX >= width || nextY >= height) {
+                    continue;
+                }
+                if (maps[nextY][nextX] == 0) {
+                    continue;
+                }
+                if (visited[nextY][nextX] != 0) {
+                    continue;
+                }
+                q.offer(new int[] {nextX, nextY});
+                visited[nextY][nextX] = visited[point[1]][point[0]] + 1;
+            }
+        }
     }
 }
