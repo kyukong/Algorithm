80d9532af14de8ae7941755661cfee28d06ad0a5
kyukong <92148749+kyukong@users.noreply.github.com>
2024-03-05 10:11:16 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,19 +1,12 @@
-select 
-    users.user_id user_id,
-    users.nickname nickname,
-    users.city || ' ' 
-        || users.street_address1 || ' ' 
-        || users.street_address2 "전체주소",
-    SUBSTR(users.tlno, 1, 3) || '-' 
-        || SUBSTR(users.tlno, 4, 4) || '-' 
-        || SUBSTR(users.tlno, 8) AS "전화번호"
-from (select 
-        writer_id, 
-        count(*) count
-    from used_goods_board
-    group by writer_id) counts
-join used_goods_user users
-on counts.writer_id = users.user_id
-where counts.count >= 3
-order by users.user_id desc
+SELECT USER_ID, NICKNAME, 
+    CITY || ' ' || STREET_ADDRESS1 || ' ' || STREET_ADDRESS2 "전체주소", 
+    SUBSTR(TLNO, 1, 3) || '-' || SUBSTR(TLNO, 4, 4) || '-' || SUBSTR(TLNO, 8) "전화번호"
+FROM (
+    SELECT WRITER_ID, COUNT(*) COUNT
+    FROM USED_GOODS_BOARD
+    GROUP BY WRITER_ID
+    HAVING COUNT(*) >= 3) BOARD
+JOIN USED_GOODS_USER USERS
+    ON BOARD.WRITER_ID = USERS.USER_ID
+ORDER BY WRITER_ID DESC
 ;
