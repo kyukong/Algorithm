e2042ba90f91e798a93a24fdd28fd953bf7fd4b2
kyukong <92148749+kyukong@users.noreply.github.com>
2024-03-05 09:58:02 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,15 +1,9 @@
-select 
-    '/home/grep/src/'
-        || board_id || '/'
-        || file_id
-        || file_name 
-        || file_ext FILE_PATH
-from used_goods_file
-where board_id = (
-    select board_id
-    from used_goods_board
-    where views = (
-        select max(views)
-        from used_goods_board))
-order by file_id desc
+SELECT '/home/grep/src/' || F.BOARD_ID || '/' || F.FILE_ID || FILE_NAME || FILE_EXT FILE_PATH
+FROM USED_GOODS_BOARD B
+JOIN USED_GOODS_FILE F
+    ON B.BOARD_ID = F.BOARD_ID
+WHERE VIEWS = (
+    SELECT MAX(VIEWS)
+    FROM USED_GOODS_BOARD)
+ORDER BY F.FILE_ID DESC
 ;
