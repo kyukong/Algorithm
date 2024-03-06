608de2ace876d1cc94d84b94a408336e45408141
kyukong <92148749+kyukong@users.noreply.github.com>
2024-03-06 09:54:05 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,10 +1,12 @@
-select name, datetime
-from (
-    select ins.*
-    from animal_ins ins
-    left join animal_outs outs
-        on ins.animal_id = outs.animal_id
-    where outs.animal_id is null
-    order by ins.datetime) animal
-where rownum <= 3
+SELECT NAME, DATETIME
+FROM (
+    SELECT ROWNUM RN, NAME, DATETIME
+    FROM (
+        SELECT INS.ANIMAL_ID, INS.NAME, INS.DATETIME
+        FROM ANIMAL_INS INS
+        LEFT JOIN ANIMAL_OUTS OUTS
+            ON INS.ANIMAL_ID = OUTS.ANIMAL_ID
+        WHERE OUTS.DATETIME IS NULL
+        ORDER BY INS.DATETIME))
+WHERE RN <= 3
 ;
