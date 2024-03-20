422b342db8456ef6403e0c13c922032f57370d29
kyukong <92148749+kyukong@users.noreply.github.com>
2024-03-20 18:34:22 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,19 +1,12 @@
--- 조건 : 2022 년 3 월 오프라인/온라인 상품 판매 데이터
--- 데이터 : 판매 날짜, 상품 아이디, 유저 아이디, 판매량
--- 데이터 : 오프라인은 user_id null 로 표시
--- 정렬 : 판매일 오름차순, 상품 아이디 오름차순, 유저 아이디 오름차순
-
-select
-    to_char(sales_date, 'YYYY-MM-DD') sales_date,
-    product_id,
-    user_id,
-    sales_amount
-from (
-    select sales_date, product_id, user_id, sales_amount
-    from online_sale
-    union
-    select sales_date, product_id, null user_id, sales_amount
-    from offline_sale)
-where to_char(sales_date, 'YYYY-MM') = '2022-03'
-order by sales_date, product_id, user_id
+SELECT TO_CHAR(SALES_DATE, 'YYYY-MM-DD') SALES_DATE, 
+    PRODUCT_ID, USER_ID, SUM(SALES_AMOUNT) SALES_AMOUNT
+FROM (
+    SELECT SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
+    FROM ONLINE_SALE
+    UNION ALL
+    SELECT SALES_DATE, PRODUCT_ID, NULL USER_ID, SALES_AMOUNT
+    FROM OFFLINE_SALE)
+WHERE TO_CHAR(SALES_DATE, 'YYYY-MM') = '2022-03'
+GROUP BY SALES_DATE, PRODUCT_ID, USER_ID
+ORDER BY SALES_DATE, PRODUCT_ID, USER_ID
 ;
