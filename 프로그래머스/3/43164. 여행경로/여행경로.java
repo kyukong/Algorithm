a90116e25c1f1cf6dc7007c00c0fadbd1e29119c
kyukong <92148749+kyukong@users.noreply.github.com>
2024-10-06 17:35:45 +0900

    [level 3] Title: 여행경로, Time: 39.37 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,53 +2,60 @@ import java.util.*;
 
 class Solution {
     
+    private static final String START = "ICN";
+    
     private String[][] tickets;
     private int[] visited;
-    private List<List<String>> results = new ArrayList<>();
+    private List<List<String>> result = new ArrayList<>();
     
     public String[] solution(String[][] tickets) {
-        // tickets 사전순 정렬
-        sort(tickets);
-        this.tickets = tickets;
+        this.tickets = sort(tickets);
         this.visited = new int[tickets.length];
         
-        // 여행경로 찾기
-        List<String> result = new ArrayList<>();
-        result.add("ICN");
-        dfs("ICN", result);
+        List<String> paths = new ArrayList<>();
+        paths.add(START);
+        dfs(START, paths);
         
-        return results.get(0).toArray(new String[tickets.length + 1]);
+        String[] answer = new String[tickets.length + 1];
+        for (int i = 0; i < tickets.length + 1; i++) {
+            answer[i] = result.get(0).get(i);
+        }
+        return answer;
     }
     
-    private void sort(String[][] tickets) {
-        Arrays.sort(tickets, (a, b) -> {
-            int result = a[0].compareTo(b[0]);
+    // 알파벳 순으로 오름차순 정렬
+    private String[][] sort(String[][] array) {
+        Arrays.sort(array, (o1, o2) -> {
+            int result = o1[0].compareTo(o2[0]);
             if (result == 0) {
-                return a[1].compareTo(b[1]);
+                return o1[1].compareTo(o2[1]);
             }
             return result;
         });
+        return array;
     }
     
-    private void dfs(String now, List<String> result) {
-        
-        if (result.size() == tickets.length + 1) {
-            results.add(new ArrayList<>(result));
+    private void dfs(String area, List<String> paths) {
+        if (paths.size() == tickets.length + 1) {
+            result.add(new ArrayList<>(paths));
             return;
         }
         
         for (int i = 0; i < tickets.length; i++) {
-            if (visited[i] == 1 || !tickets[i][0].equals(now)) {
+            if (visited[i] == 1) {
+                continue;
+            }
+            if (!tickets[i][0].equals(area)) {
                 continue;
             }
             
             visited[i] = 1;
-            result.add(tickets[i][1]);
+            paths.add(tickets[i][1]);
             
-            dfs(tickets[i][1], result);
+            dfs(tickets[i][1], paths);
             
-            result.remove(result.size() - 1);
             visited[i] = 0;
+            paths.remove(paths.size() - 1);
         }
     }
 }
