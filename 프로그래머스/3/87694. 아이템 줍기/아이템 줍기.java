9a8c290aec4769d1dfb5e867affe949decffbbdf
kyukong <92148749+kyukong@users.noreply.github.com>
2024-10-06 19:14:10 +0900

    [level 3] Title: 아이템 줍기, Time: 1.42 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -2,83 +2,84 @@ import java.util.*;
 
 class Solution {
     
-    private static final int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private static final int[][] directions
+        = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    private static final int SIZE = 102;
     
-    private int[][] map = new int[101][101];
-    private int[][] visited = new int[101][101];
+    private int[][] board = new int[SIZE][SIZE];
+    private int[][] visited = new int[SIZE][SIZE];
+    private int[][] map = new int[SIZE][SIZE];
+    
     private int[][] rectangle;
     private int characterX;
     private int characterY;
     private int itemX;
     private int itemY;
-    private int result = Integer.MAX_VALUE;
     
-    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+    private int answer = Integer.MAX_VALUE;
+    
+    public int solution(int[][] rectangle, 
+                        int characterX, int characterY, 
+                        int itemX, int itemY) {
         this.rectangle = rectangle;
         this.characterX = characterX * 2;
         this.characterY = characterY * 2;
         this.itemX = itemX * 2;
         this.itemY = itemY * 2;
         
-        // rectangle 값 모두 2배로 변경
-        for (int[] rect : rectangle) {
-            for (int i = 0; i < 4; i++) {
-                rect[i] *= 2;
-            }
-        }
+        fill();
+        bfs();
         
-        // 테두리는 1 로 채우기
-        for (int[] rect : rectangle) {
-            // 아래 & 위
-            for (int i = rect[0]; i <= rect[2]; i++) {
-                map[rect[1]][i] = 1;
-                map[rect[3]][i] = 1;
-            }
+        return map[this.itemY][this.itemX] / 2;
+    }
+    
+    private void fill() {
+        for (int[] point : rectangle) {
+            int leftX = point[0] * 2;
+            int leftY = point[1] * 2;
+            int rightX = point[2] * 2;
+            int rightY = point[3] * 2;
             
-            // 왼쪽 & 오른쪽
-            for (int i = rect[1]; i <= rect[3]; i++) {
-                map[i][rect[0]] = 1;
-                map[i][rect[2]] = 1;
-            }
-        }
-        
-        // 도형 내부는 0 으로 채우기
-        for (int[] rect : rectangle) {
-            for (int y = rect[1] + 1; y <= rect[3] - 1; y++) {
-                for (int x = rect[0] + 1; x <= rect[2] - 1; x++) {
-                    map[y][x] = 0;
+            for (int y = leftY; y <= rightY; y++) {
+                for (int x = leftX; x <= rightX; x++) {
+                    int color = 2;
+                    if (y == leftY || y == rightY
+                        || x == leftX || x == rightX) {
+                        color = 1;
+                    }
+                    
+                    // 선: 1, 내부: 2
+                    board[y][x] = Math.max(board[y][x], color);
                 }
             }
         }
-        
-        bfs();
-        
-        return map[itemY * 2][itemX * 2] / 2;
     }
     
     private void bfs() {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {characterX, characterY});
-        visited[characterY][characterX] = 1;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {characterY, characterX});
         
-        while (!q.isEmpty()) {
-            int[] now = q.poll();
-            if (now[0] == itemX && now[1] == itemY) {
+        visited[characterY][characterX] = 1;
+        map[characterY][characterX] = 1;
+        
+        while (!queue.isEmpty()) {
+            int[] point = queue.poll();
+            if (point[1] == itemX && point[0] == itemY) {
                 break;
             }
             
             for (int[] direction : directions) {
-                int nextX = now[0] + direction[0];
-                int nextY = now[1] + direction[1];
+                int nextX = point[1] + direction[1];
+                int nextY = point[0] + direction[0];
                 
-                if (nextX < 0 || nextY < 0 || nextX > 100 || nextY > 100) {
+                if (nextX < 0 || nextX >= SIZE || nextY < 0 || nextY >= SIZE) {
                     continue;
                 }
-                if (map[nextY][nextX] != 1 || visited[nextY][nextX] == 1) {
+                if (board[nextY][nextX] != 1 || visited[nextY][nextX] == 1) {
                     continue;
                 }
-                map[nextY][nextX] = map[now[1]][now[0]] + 1;
-                q.offer(new int[] {nextX, nextY});
+                map[nextY][nextX] = map[point[0]][point[1]] + 1;
+                queue.offer(new int[] {nextY, nextX});
                 visited[nextY][nextX] = 1;
             }
         }
