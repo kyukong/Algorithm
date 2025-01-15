67f436a6c2151acd329eb6ccb11cac5ba1ebcea2
kyukong <92148749+kyukong@users.noreply.github.com>
2025-01-15 21:27:11 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 140 ms, Memory: 22888 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final int[] visited = new int[100_002];
+
+    private static int N;
+    private static int K;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(N);
+        // 시작 지점을 1칸 이동으로 취급
+        visited[N] = 1;
+
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+            if (now == K) {
+                System.out.println(visited[K] - 1);
+                return;
+            }
+
+            for (int next : new int[] {now * 2, now - 1, now + 1}) {
+                if (next < 0 || next >= visited.length - 1) {
+                    continue;
+                }
+                if (visited[next] != 0) {
+                    continue;
+                }
+
+                if (next == now * 2) {
+                    visited[next] = visited[now];
+                } else {
+                    visited[next] = visited[now] + 1;
+                }
+                queue.offer(next);
+            }
+        }
+    }
+}
