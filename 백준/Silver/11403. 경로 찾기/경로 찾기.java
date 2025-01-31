000b1e7245b7e5e64827d4dca37949a8263d757e
kyukong <92148749+kyukong@users.noreply.github.com>
2025-01-31 21:07:52 +0900

    [Silver I] Title: 경로 찾기, Time: 184 ms, Memory: 16844 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int N;
+    private static int[][] distance;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        distance = new int[N][N];
+
+        // 초기화
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                distance[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (distance[i][k] == 1 && distance[k][j] == 1) {
+                        distance[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        // 결과
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(distance[i][j])
+                    .append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
